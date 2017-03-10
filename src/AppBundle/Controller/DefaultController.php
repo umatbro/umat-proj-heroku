ca06a56a57e8153f55ccce83a6f39bbf60582131
umatbro <umatbroo@gmail.com>
2017-03-10 14:36:26 +0100

    simple le form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,25 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/register", name="register")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(){
+        return $this->render('mat/register.html.twig');
+    }
+
+    /**
+     * @Route("/confirmation", name="confirm")
+     */
+    public function confirmAction(){
+        $x = $_POST['name'];
+        return $this->render('mat/confirmation.html.twig', array(
+            'imie' => $x
+        ));
+    }
+
 }
