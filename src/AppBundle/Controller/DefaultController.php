153131d3ddfcaee02610e2f4b5415291d209e3ab
student <student@lab404-12.lab.metal.agh.edu.pl>
2017-04-10 10:40:33 +0200

    display products on main page, z zajec

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+		
+		$em = $this -> getDoctrine() -> getManager();
+		$products = $em -> find('AppBundle:Product');
+		
+		foreach($products as $product){
+			$productNames[] = $product-> getName();
+		}
+		
+		
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig', ['productNames' => $productNames]);
     }
     //
 }
