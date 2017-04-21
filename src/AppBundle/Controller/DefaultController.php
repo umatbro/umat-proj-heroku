1d5d2dbcbf2dfebcc8694c78732b81848ae99821
umatbro <umatbroo@gmail.com>
2017-04-21 23:57:37 +0200

    basic adding numbers to cart

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\OrderItem;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,4 +27,48 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', ['products' => $products]);
     }
     //
+
+    /**
+     * @Route("/add-order-item/{productID}", name="addOrderItem")
+     */
+    public function addOrderItemAction($productID){
+//        $session = $this->get('session');
+//        $session->set('quantity');
+
+        $em = $this -> getDoctrine() -> getManager();
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        foreach($products as $product){
+            $productNames[] = $product-> getName();
+        }
+
+
+        if(!isset($_SESSION['quantity'])) $_SESSION['quantity'] = 1;
+        $_SESSION['quantity']++;
+        $session = $this->get('session');
+        $session -> set('filter', array(
+            'quantity' => $_SESSION['quantity']
+        ));
+        return $this->render('default/index.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("/clear-card",name="clearCard")
+     */
+    public function clearCard(){
+        $em = $this -> getDoctrine() -> getManager();
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        foreach($products as $product) {
+            $productNames[] = $product->getName();
+        }
+
+        if(isset($_SESSION['quantity'])) ($_SESSION['quantity'] = 0);
+        $session = $this->get('session');
+        $session -> set('filter', array(
+            'quantity' => $_SESSION['quantity']
+        ));
+
+        return $this->render('default/index.html.twig', ['products'=>$products]);
+    }
 }
