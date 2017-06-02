940778c54085d3bd26961f062b645c1adc4bd574
umatbro <umatbroo@gmail.com>
2017-06-02 12:34:22 +0200

    prepared "test" route to prepare for new entity structure tests

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -25,6 +25,13 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', ['products' => $products]);
     }
-    //
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(){
+        return $this->render('default/test.html.twig');
+    }
+
 
 }
