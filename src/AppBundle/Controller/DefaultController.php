8f16bda9e716c73344e2ffa8be748edf7ef01352
umatbro <umatbroo@gmail.com>
2017-06-04 01:22:59 +0200

    styled order summary page
    TODO: add payment link and controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,49 +23,6 @@ class DefaultController extends Controller
 			$productNames[] = $product-> getName();
 		}
 		
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', ['products' => $products]);
     }
-
-    /**
-     * @Route("/test", name="test")
-     */
-    public function testAction(){
-        $session = $this -> get('session');
-        $orderItems = $session->get('orderItems');
-        $em = $this->getDoctrine()->getManager();
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
-
-        $repository = $this->getDoctrine()->getRepository('AppBundle:Product');
-        $userOrder = new UserOrder();
-        $totalPrice = 0;
-
-        foreach($orderItems as $orderItem){
-            $orderItem->setUser($usr);
-            $product = $repository->find($orderItem->getProduct()->getId());
-            $orderItem->setProduct($product);
-            $category = $product->getCategory();
-            $orderItem->getProduct()->setCategory($category);
-            $userOrder->addOrderItem($orderItem);
-            $totalPrice += ($orderItem -> getNumberOfProducts()) * ($orderItem -> getProduct() -> getDefaultPrice());
-        }
-        foreach($orderItems as $orderItem){
-            $orderItem->setUserOrder($userOrder);
-            $em->persist($orderItem);
-        }
-        $userOrder->setTotalPrice($totalPrice);
-        $em->persist($userOrder);
-        $em->flush();
-        $em->clear();
-
-
-
-        return $this->render('default/test.html.twig',["orderItems"=> $orderItems,
-            "username"=> $usr->getUsername(),
-            "category" => $category,
-            "userOrder" => $userOrder
-            ]);
-    }
-
-
 }
