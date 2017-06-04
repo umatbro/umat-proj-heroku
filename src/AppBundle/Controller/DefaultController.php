2205f0ad1badee7f82896a215842895f07d4907f
umatbro <umatbroo@gmail.com>
2017-06-05 00:51:57 +0200

    adding viewing orders from user profile level

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -25,4 +25,30 @@ class DefaultController extends Controller
 		
         return $this->render('default/index.html.twig', ['products' => $products]);
     }
+
+    /**
+     * @Route("/profile/orders", name="profile_orders")
+     */
+    public function profileOrdersAction(){
+        $em = $this->getDoctrine()->getManager();
+        $user= $this->get('security.token_storage')->getToken()->getUser();
+        $orderItems = $em->getRepository('AppBundle\Entity\OrderItem') -> findBy([
+            'user' => $user->getId()
+        ]);
+
+        $userOrders = [];
+        $searched = [];
+        foreach($orderItems as $item){
+            $orderId = $item->getUserOrder()->getId();
+            if(!in_array($orderId, $searched))
+                $userOrders[] = $em->getRepository("AppBundle\Entity\UserOrder") -> find($orderId);
+            $searched[] = $orderId;
+        }
+
+        return $this->render('@FOSUser/Profile/show_orders.html.twig',[
+            'user' => $user,
+            'orderItems' => $orderItems,
+            'orders' => $userOrders
+        ]);
+    }
 }
